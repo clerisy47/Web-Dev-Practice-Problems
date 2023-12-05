fdf4a2308f61619e5a3f55168726e45916a1dc28
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2023-12-05 11:20:38 +0545

    added oop structure for the game

--- Simon-game/game.js
+++ Simon-game/game.js
@@ -1,13 +1,44 @@
-let colors = ["red", "blue", "green", "yellow"]
-let pattern = []
-pattern.push(colors[Math.floor(Math.random()*4)])
+class Game{
+    constructor(){
+        this.colors = ["red", "blue", "green", "yellow"]
+        this.gamePattern = []
+        this.userPattern = []
+        this.level = 1
+        this.gamePattern.push(this.colors[Math.floor(Math.random()*4)])
+        this.body = document.querySelector('body')
+        this.buttons = document.querySelectorAll(".btn")
+        this.levelTitle = document.querySelector("#level-title")
+        this.initializeGame()
+        this.gameStarted = false
+    }
+    initializeGame(){
+        this.body.addEventListener("keydown", ()=>{
+            if (! this.gameStarted){
+                this.levelTitle.textContent = 'Level 1'
+                this.gameStarted= true
+                this.startGame()
+            }
+        })
+    }
+    startGame(){
+        this.buttons.forEach((button) => {
+            button.addEventListener("click", ()=>{
+                button.classList.add("pressed")
+                let sound = new Audio("sounds/"+ button.id+".mp3")
+                sound.play()
+                setTimeout(()=>button.classList.remove("pressed")
+                ,100)
+                this.userPattern.push(button.id)
+                this.level += 1
+                this.levelTitle.textContent = "level " + this.level
+            })
+        })
+    }
+}
 
-let currentButton = document.querySelector("#"+ pattern[pattern.length-1])
+let game = new Game()
 
-currentButton.addEventListener("click", ()=> {
-    currentButton.classList.add("pressed")
-    let sound = new Audio("sounds/"+ pattern[pattern.length-1]+".mp3")
-    sound.play()
-    setTimeout(()=>currentButton.classList.remove("pressed")
-    ,100)
-})
+
+
+
+
