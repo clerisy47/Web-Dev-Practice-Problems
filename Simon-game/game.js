8f4b4074b4dff506f04e03eb7dd76de99d2be787
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2023-12-06 10:21:26 +0545

    fixed bugs

--- Simon-game/game.js
+++ Simon-game/game.js
@@ -1,61 +1,62 @@
-class Game{
-    constructor(){
-        this.colors = ["red", "blue", "green", "yellow"]
-        this.gamePattern = []
-        this.userPattern = []
-        this.level = 1
-        this.body = document.querySelector('body')
-        this.buttons = document.querySelectorAll(".btn")
-        this.levelTitle = document.querySelector("#level-title")
-        this.initializeGame()
-        this.gameRunning = false
-    }
-    randomColorGenerator(){
-        let color = this.colors[Math.floor(Math.random()*4)]
-        this.gamePattern.push(color)
-        this.colorAnimation(document.querySelector("#"+color))
-    }
-    colorAnimation(button){
-        button.classList.add("pressed")
-        let sound = new Audio("sounds/"+ button.id+".mp3")
-        sound.play()
-        setTimeout(()=>button.classList.remove("pressed")
-        ,100)
-    }
-    initializeGame(){
-        this.body.addEventListener("keydown", ()=>{
-            if (! this.gameRunning){
-                this.levelTitle.textContent = 'Level 1'
-                this.gameRunning= true
-                this.nextSequence()
-            }
-        })
-    }
-    nextSequence(){
-            this.randomColorGenerator()
-            this.buttons.forEach((button) => {
-                button.addEventListener("click", ()=>{
-                    this.colorAnimation(button)
-                    this.userPattern.push(button.id)
-                    this.level += 1
-                    this.levelTitle.textContent = "level " + this.level
-                })
-            this.checkColor()
-        })}
-    checkColor(){
-        if (this.userPattern= this.gamePattern){
-            alert("Good, its matched")
-            this.userPattern = []
-            this.nextSequence()
+const colors = ["red", "blue", "green", "yellow"];
+let gamePattern = [];
+let userPattern = [];
+let level = 1;
+const body = document.querySelector('body');
+const buttons = document.querySelectorAll(".btn");
+const levelTitle = document.querySelector("#level-title");
+let gameRunning = false;
+
+
+function initializeGame() {
+    body.addEventListener("keydown", () => {
+        if (!gameRunning) {
+            levelTitle.textContent = 'Level 1';
+            gameRunning = true;
+            randomColorGenerator();
         }
-        else{
-            alert("Bad, it didn't matched")
-        }
+    });
+}
+
+function randomColorGenerator() {
+    let color = colors[Math.floor(Math.random() * 4)];
+    gamePattern.push(color);
+    colorAnimation(document.querySelector("#" + color));
+}
+
+function colorAnimation(button) {
+    button.classList.add("pressed");
+    let sound = new Audio("sounds/" + button.id + ".mp3");
+    sound.play();
+    setTimeout(() => button.classList.remove("pressed"), 100);
+}
+
+function eventListener() {
+    for (const button of buttons) {
+        button.addEventListener("click", () => {
+            colorAnimation(button);
+            userPattern.push(button.id);
+            levelTitle.textContent = "level " + level;
+            checkColor();
+        });
     }
+}
+
+function checkColor() {
+    if (userPattern.length != gamePattern.length){
+        eventListener()
     }
-let game = new Game()
+    else if (JSON.stringify(userPattern) === JSON.stringify(gamePattern)) {
+        userPattern = [];
+        level += 1;
+        randomColorGenerator();
+    } else {
+        levelTitle.textContent = "Game Over"
+        gameRunning = false;
+        level = 1;
+        userPattern = [];
+    }
+}
 
 
-
-
-
+initializeGame();
