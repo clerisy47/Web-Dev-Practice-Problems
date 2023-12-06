f0cbf4c92e535fa6180db4fcd53f0b9e048a3fd0
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2023-12-06 20:00:58 +0545

    changed check color function

--- Simon-game/game.js
+++ Simon-game/game.js
@@ -1,62 +1,59 @@
-const colors = ["red", "blue", "green", "yellow"];
-let gamePattern = [];
-let userPattern = [];
-let level = 1;
-const body = document.querySelector('body');
-const buttons = document.querySelectorAll(".btn");
-const levelTitle = document.querySelector("#level-title");
-let gameRunning = false;
+let buttons = ["red", "blue", "green", "yellow"];
+let userPattern = []
+let gamePattern = []
+let level = 0
+let gameRunning = false
+let levelTitle = document.querySelector("#level-title")
 
-
-function initializeGame() {
-    body.addEventListener("keydown", () => {
-        if (!gameRunning) {
-            levelTitle.textContent = 'Level 1';
-            gameRunning = true;
-            randomColorGenerator();
-        }
-    });
+function nextSequence(){
+    userPattern = []
+    level++
+    document.querySelector("#level-title").textContent = "Level "+ level
+    let randomNumber = Math.floor(Math.random()*4)
+    let randomColor = buttons[randomNumber]
+    playAnimation(document.querySelector('#'+ randomColor))
+    gamePattern.push(randomColor)
 }
 
-function randomColorGenerator() {
-    let color = colors[Math.floor(Math.random() * 4)];
-    gamePattern.push(color);
-    colorAnimation(document.querySelector("#" + color));
-}
-
-function colorAnimation(button) {
-    button.classList.add("pressed");
-    let sound = new Audio("sounds/" + button.id + ".mp3");
-    sound.play();
-    setTimeout(() => button.classList.remove("pressed"), 100);
-}
-
-function eventListener() {
-    for (const button of buttons) {
-        button.addEventListener("click", () => {
-            colorAnimation(button);
-            userPattern.push(button.id);
-            levelTitle.textContent = "level " + level;
-            checkColor();
-        });
-    }
+function playAnimation(button) {
+    button.classList.add("pressed")
+    let sound = new Audio("sounds/" + button.id + ".mp3")
+    sound.play()
+    setTimeout(() => button.classList.remove("pressed"), 100)
 }
 
 function checkColor() {
     if (userPattern.length != gamePattern.length){
-        eventListener()
+        nextSequence()
     }
     else if (JSON.stringify(userPattern) === JSON.stringify(gamePattern)) {
-        userPattern = [];
-        level += 1;
-        randomColorGenerator();
+        userPattern = []
+        level += 1
+        setTimeout(()=>{
+            nextSequence()
+        },1000)
     } else {
         levelTitle.textContent = "Game Over"
-        gameRunning = false;
-        level = 1;
-        userPattern = [];
+        gameRunning = false
+        level = 1
+        userPattern = []
+
     }
 }
 
+document.querySelector('body').addEventListener("keydown", () => {
+    if (!gameRunning) {
+        levelTitle.textContent = 'Level 1'
+        gameRunning = true
+        nextSequence()
+    }
+});
 
-initializeGame();
+for (const button of document.querySelectorAll(".btn")) {
+        button.addEventListener("click", () => {
+        playAnimation(button);
+        userPattern.push(button.id);
+        levelTitle.textContent = "level " + level;
+        checkColor();
+    });
+}
